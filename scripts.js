96a3551fe9d2e5330a0adcd1de6a432ae4936999
PPableo <pableop@gmail.com>
2024-02-25 01:05:39 -0500

    added testimonial section, and slider effect

--- scripts.js
+++ scripts.js
@@ -72,3 +72,29 @@ ScrollReveal().reveal(".about__content .about__btn", {
   ...scrollRevealOption,
   delay: 2000,
 });
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  // speed: 400,
+  // spaceBetween: 100,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+swiper();
